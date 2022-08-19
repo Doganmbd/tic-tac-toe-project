9e9c3a6dc73755a53c69fcb5e2ef1da77e46c885
Mustafa Bilal Dogan <mbilaldogan90@gmail.com>
2022-08-19 09:11:37 +0300

    v4

--- src/contexts/GameContext.js
+++ src/contexts/GameContext.js
@@ -4,8 +4,11 @@ export const GameContext = createContext();
 
 const GameContextProvider = ({children}) => {
 
+    const [squares, setSquares] = useState(Array(9).fill(null));
     const state = {
-
+        squares,
+        setSquares,
+        
       };
 
   return <GameContext.Provider value={state}>{children}</GameContext.Provider>;
